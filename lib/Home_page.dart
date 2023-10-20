696ef7626a7b04cfe2124943f6044c42db94d860
arunk19222 <arunk19222@gmail.com>
2023-10-20 08:58:59 +0530

    Update Home_page.dart

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -311,7 +311,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-print("Sds");
     return GestureDetector(
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
